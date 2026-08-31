b3fa3a69fd46170f376f5ad6c3125f0d6117b59c
Marius Gauland <marius.gauland@mybits.de>
2026-08-31 11:06:47 +0200

    feat: add create abrechnung start

---
+++ frontend/src/routes/projekte/[projektId]/abrechnungen/anlegen.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-btn
+      class="mb-2"
+      :prepend-icon="mdiArrowLeft"
+      variant="text"
+      :to="`/projekte/${projektId}/abrechnungen`"
+    >
+      Zurück zu den Abrechnungen
+    </v-btn>
+
+    <h1 class="text-display-medium font-weight-bold mb-6">
+      Abrechnung anlegen
+    </h1>
+
+    <v-tabs
+      v-model="tab"
+      class="mb-4"
+    >
+      <v-tab :value="TABS.BASIS">Basisinformationen</v-tab>
+      <v-tab :value="TABS.BERECHNUNG">Berechnung</v-tab>
+    </v-tabs>
+
+    <v-tabs-window v-model="tab">
+      <v-tabs-window-item :value="TABS.BASIS" />
+      <v-tabs-window-item :value="TABS.BERECHNUNG" />
+    </v-tabs-window>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { mdiArrowLeft } from "@mdi/js";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const TABS = {
+  BASIS: "basis",
+  BERECHNUNG: "berechnung",
+} as const;
+
+const route = useRoute("/projekte/[projektId]/abrechnungen/anlegen");
+const projektId = route.params.projektId;
+
+const tab = ref<string>(TABS.BASIS);
+</script>
