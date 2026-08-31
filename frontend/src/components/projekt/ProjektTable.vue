2f0c75bae0614c222cb54ab4416df3e5151294c2
Marius Gauland <marius.gauland@mybits.de>
2026-08-31 11:06:47 +0200

    feat: add create abrechnung start

--- frontend/src/components/projekt/ProjektTable.vue
+++ frontend/src/components/projekt/ProjektTable.vue
@@ -10,13 +10,25 @@
     :loading="loading"
     item-value="id"
     :no-data-text="noDataText"
-  />
+  >
+    <template #[`item.abrechnungen`]="{ item }">
+      <v-btn
+        v-if="item.id"
+        :aria-label="`Abrechnungen zu Projekt ${item.projektnummer} anzeigen`"
+        density="comfortable"
+        :icon="mdiReceiptTextOutline"
+        :to="`/projekte/${item.id}/abrechnungen`"
+        variant="text"
+      />
+    </template>
+  </v-data-table-server>
 </template>
 
 <script setup lang="ts">
 import type { DataTableSortItem } from "@/types/DataTableSortItem";
 import type { ProjektTableRow } from "@/types/ProjektTableRow";
 
+import { mdiReceiptTextOutline } from "@mdi/js";
 import { computed } from "vue";
 
 const HEADERS = [
@@ -24,6 +36,7 @@ const HEADERS = [
   { title: "Abrechnung Beginn", key: "abrechnungBeginn" },
   { title: "Abrechnung Ende", key: "abrechnungEnde" },
   { title: "Adressen/Flurnummern", key: "anzahlAdressen", sortable: false },
+  { title: "Abrechnungen", key: "abrechnungen", sortable: false },
 ];
 
 const ITEMS_PER_PAGE_OPTIONS = [10, 25, 50, 100];
